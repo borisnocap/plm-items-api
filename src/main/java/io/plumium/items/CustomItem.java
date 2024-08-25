76143bab66327e566ae4848c582506ee587fdcfa
PC <@pc>
2024-08-25 13:16:32 +0300

    Тест клонирования

--- src/main/java/io/plumium/items/CustomItem.java
+++ src/main/java/io/plumium/items/CustomItem.java
@@ -9,57 +9,38 @@ import org.jetbrains.annotations.Nullable;
 
 import static net.kyori.adventure.text.Component.text;
 
-public class CustomItem extends ItemStack {
+public class CustomItem extends ItemStack implements Cloneable {
 
-    protected String id;
-    protected String name;
-    protected Rarity rarity;
-    protected ItemMeta itemMeta;
-    protected Component displayName;
+    private final String id;
+    private final String name;
+    private final Rarity rarity;
+    private ItemMeta itemMeta;
+    private Component displayName;
 
-    public CustomItem(@NotNull final Material type,
+    public CustomItem(@NotNull ItemsAPI itemsAPI,
+                      @NotNull final Material type,
                       @NotNull final String id,
                       @NotNull final String name,
-                      @NotNull final Rarity rarity,
-                      @NotNull ItemsAPI itemsAPI) {
+                      @NotNull final Rarity rarity) {
         super(type);
         itemMeta = super.getItemMeta();
         this.id = id;
         this.name = name;
         this.rarity = rarity;
-        displayName = text()
-                .color(rarity.getColor())
-                .append(text("["))
-                .append(text(name))
-                .append(text("]"))
-                .hoverEvent(asHoverEvent())
-                .build();
+        displayName = generateDisplayName();
+
+
         itemsAPI.registerCustomItem(this);
     }
 
-    @Override
-    public @NotNull Component displayName() {
-        return displayName;
-    }
-
-    @Override
-    public @NotNull ItemMeta getItemMeta() {
-        return itemMeta;
-    }
-
-    @Override
-    public boolean setItemMeta(@Nullable ItemMeta itemMeta) {
-        boolean result = super.setItemMeta(itemMeta);
-        if (!result) return false;
-        this.itemMeta = itemMeta;
-        displayName = text()
+    private Component generateDisplayName() {
+        return text()
                 .color(rarity.getColor())
                 .append(text("["))
                 .append(text(name))
                 .append(text("]"))
                 .hoverEvent(asHoverEvent())
                 .build();
-        return true;
     }
 
     public String getId() {
@@ -68,5 +49,36 @@ public class CustomItem extends ItemStack {
 
     public Rarity getCustomRarity() {
         return rarity;
+    }
+
+    @Override
+    public @NotNull Component displayName() {
+        return displayName;
+    }
+
+    /**
+     * При вызове getItemMeta() у ItemStack возвращается копия ItemMeta.
+     * Чтобы не создавать копию для каждого вызова getItemMeta у CustomItem, я кэширую актуальную мету.
+     */
+    @Override
+    public @NotNull ItemMeta getItemMeta() {
+        return itemMeta;
+    }
+
+    /**
+     * При изменении меты у CustomItem мне нужно сохранить актуальное значение в кэш.
+     */
+    @Override
+    public boolean setItemMeta(@Nullable ItemMeta itemMeta) {
+        boolean result = super.setItemMeta(itemMeta);
+        if (!result) return false;
+        this.itemMeta = itemMeta;
+        displayName = generateDisplayName();;
+        return true;
+    }
+
+    @Override
+    public @NotNull CustomItem clone() {
+        return (CustomItem) super.clone();
     }
 }
