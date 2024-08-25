dabb4f8643dfd2eaccf093365dd33b4e6716a401
PC <@pc>
2024-08-25 16:48:04 +0300

    Убраны тела у дефолтных методов. Эти методы будут реализованы только в плагине PLM-Items.

--- src/main/java/ru/plm/items/ItemsAPI.java
+++ src/main/java/ru/plm/items/ItemsAPI.java
@@ -1,7 +1,6 @@
 package ru.plm.items;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.TextColor;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataType;
@@ -17,82 +16,14 @@ public interface ItemsAPI {
     NamespacedKey ITEM_RARITY_KEY = new NamespacedKey("plm-items", "rarity");
     PersistentDataType<String, String> ITEM_RARITY_KEY_TYPE = PersistentDataType.STRING;
 
-    default boolean isCustom(ItemStack item) {
-        // Если у предмета есть запись об ID, тогда это кастомный предмет.
-        return item.getPersistentDataContainer().has(ITEM_ID_KEY,  PersistentDataType.STRING);
-    }
+    boolean isCustom(ItemStack item);
 
     @Nullable
-    default String getID(ItemStack item) {
-        return item.getPersistentDataContainer().get(ITEM_ID_KEY,  PersistentDataType.STRING);
-    }
+    String getID(ItemStack item);
 
-    default Rarity getRarity(ItemStack item) {
-        String rarity = item.getPersistentDataContainer().get(ITEM_RARITY_KEY,  PersistentDataType.STRING);
-        /*
-        Если нет записи о редкости, то это стандартный предмет Minecraft и его редкость определяется на основе
-        цвета его названия (на момент 1.21.1 их всего 4).
-        */
-        if (rarity == null) {
-            TextColor color = item.displayName().color();
-            // Цвет у названия предмета вроде как не бывает null, но метод помечен как @Nullable.
-            if (color == null) return Rarity.COMMON;
-            // В качестве ключей используются значения констант из NamedTextColor.
-            switch (color.value()) {
-                case 0xffff55 -> {
-                    return Rarity.UNCOMMON;
-                }
-                case 0x55ffff -> {
-                    return Rarity.RARE;
-                }
-                case 0xff55ff -> {
-                    return Rarity.EPIC;
-                }
-                default -> {
-                    return Rarity.COMMON; // 0xffffff
-                }
-            }
-        }
-        // Если запись о редкости имеется, то это кастомный предмет.
-        switch (rarity) {
-            case "uncommon" -> {
-                return Rarity.UNCOMMON;
-            }
-            case "rare" -> {
-                return Rarity.RARE;
-            }
-            case "epic" -> {
-                return Rarity.EPIC;
-            }
-            case "legendary" -> {
-                return Rarity.LEGENDARY;
-            }
-            case "artefact" -> {
-                return Rarity.ARTEFACT;
-            }
-            case "wonderful" -> {
-                return Rarity.WONDERFUL;
-            }
-            default -> {
-                return Rarity.COMMON;
-            }
-        }
-    }
+    Rarity getRarity(ItemStack item);
 
-    /**
-     * У кастомных предметов цвет имени может не совпадать с цветом скобок, которые сервер возвращает при вызове
-     * displayName().
-     * Пример: imgur.com/a/Q7V8ABx
-     * Поэтому, если предмет кастомный (у него есть запись о редкости), нужно окрасить "[Предмет]" в
-     * цвет его редкости.
-     */
-    default Component getDisplayName(ItemStack item) {
-        String rarity = item.getPersistentDataContainer().get(ITEM_RARITY_KEY,  PersistentDataType.STRING);
-        if (rarity == null) {
-            return item.displayName();
-        }
-        return item.displayName().color(getRarity(item).getColor());
-    }
+    Component getDisplayName(ItemStack item);
 
     ItemStack getItem(String customItemId);
 
--- src/main/java/ru/plm/items/Rarity.java
+++ src/main/java/ru/plm/items/Rarity.java
@@ -1,13 +1,11 @@
 package ru.plm.items;
 
-import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
-import net.kyori.adventure.text.format.TextDecoration;
 
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
+@SuppressWarnings("unused")
 public enum Rarity {
-
     COMMON(WHITE),
     UNCOMMON(YELLOW),
     RARE(AQUA),
@@ -16,19 +14,13 @@ public enum Rarity {
     ARTEFACT(RED),
     WONDERFUL(GREEN);
 
-    private final Style style;
     private final TextColor color;
 
     Rarity(TextColor color) {
         this.color = color;
-        this.style = Style.style().color(color).decoration(TextDecoration.ITALIC, false).build();
     }
 
     public TextColor getColor() {
         return color;
-    }
-
-    public Style getStyle() {
-        return style;
     }
 }
