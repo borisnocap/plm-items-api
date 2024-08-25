8194a5bed71576a0da40fe394498c38851530e79
PC <@pc>
2024-08-25 18:06:51 +0300

    Добавлен метод получения nonItalicColor.

--- src/main/java/ru/plm/items/Rarity.java
+++ src/main/java/ru/plm/items/Rarity.java
@@ -1,6 +1,8 @@
 package ru.plm.items;
 
+import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.format.TextDecoration;
 
 import static net.kyori.adventure.text.format.NamedTextColor.*;
 
@@ -15,12 +17,18 @@ public enum Rarity {
     WONDERFUL(GREEN);
 
     private final TextColor color;
+    private final Style nonItalicColor;
 
     Rarity(TextColor color) {
         this.color = color;
+        nonItalicColor = Style.style(color).decoration(TextDecoration.ITALIC, false);
     }
 
     public TextColor getColor() {
         return color;
+    }
+
+    public Style getNonItalicColor() {
+        return nonItalicColor;
     }
 }
